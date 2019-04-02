0026e6835042762f4fea98243e360d668baca140
Jorge Juvenal Campos Ferreira <41249739+JuveCampos@users.noreply.github.com>
2019-04-02 13:15:38 -0600

    Add files via upload

--- SeminarioStringR2/Scripts/setup.R
+++ SeminarioStringR2/Scripts/setup.R
@@ -1,7 +1,7 @@
 # Checar si esta instalado Pacman
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(sf, tidyverse, rebus, readr)
-Sys.setlocale("LC_ALL", 'en_US.UTF-8')
+#Sys.setlocale("LC_ALL", 'en_US.UTF-8')
 # default_locale() # Verificar que sean 
 
 # Funciones 
@@ -9,7 +9,7 @@ source("https://raw.githubusercontent.com/JuveCampos/miniProyectos/master/Semina
 niveles <- function(x) levels(as.factor(x))
 
 # Leemos Bases de Datos 
-tweets_amlo <- readRDS("BasesDeDatos/datos_tweet_amlo.rds")
-ernesto <- read.delim("BasesDeDatos/importance-of-being-earnest.txt")
-mapa <- st_read("BasesDeDatos/presas_cdmx.geojson", quiet = T)
-noticias <- read_csv("BasesDeDatos/Noticias_inundaciones.csv") %>% select(abstract, body)
+tweets_amlo <- read_csv("https://raw.githubusercontent.com/JuveCampos/miniProyectos/master/SeminarioStringR2/BasesDeDatos/tweets_amlo.csv")
+ernesto <- read.delim("https://raw.githubusercontent.com/JuveCampos/miniProyectos/master/SeminarioStringR2/BasesDeDatos/importance-of-being-earnest.txt")
+mapa <- st_read("https://raw.githubusercontent.com/JuveCampos/miniProyectos/master/SeminarioStringR2/BasesDeDatos/presas_cdmx.geojson", quiet = T)
+noticias <- read_csv("https://github.com/JuveCampos/miniProyectos/raw/master/SeminarioStringR2/BasesDeDatos/Noticias_inundaciones.csv") %>% select(abstract, body)
