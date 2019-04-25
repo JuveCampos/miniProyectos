9a9ed68567defbfd52c1c1ca9d53046d24e51685
Jorge Juvenal Campos Ferreira <41249739+JuveCampos@users.noreply.github.com>
2019-04-25 09:40:18 -0500

    findLocaleAndModPlots

--- GraficasEMAS/procesamientoDatosEMAS.R
+++ GraficasEMAS/procesamientoDatosEMAS.R
@@ -1,17 +1,19 @@
 ##################################################################
 # Plot Series de tiempo de Estaciones Meteorologicas Automaticas #
 ##################################################################
+Sys.setlocale("LC_ALL", 'en_US.UTF-8')
 
-# Codigo valido solo para estaciones con 10 variables
+# Codigo valido solo para estaciones con 10 variables (falta generalizar un poco más).
+# Caso: Estacion C. Biologicas IPN (Más céntrica al centro de la CDMX disponible).
 
-# Librerias
+# Librerias y funciones
 library(tidyverse)
 library(stringr)
 library(rebus)
 captura <- function(x) capture(one_or_more(x))
 
 # Leemos datos
-datos <- read.delim("https://smn.cna.gob.mx/tools/GUI/EMAS_reporte_v2.php?id=616&formato=txt&tipo=90d") %>% 
+datos <- read.delim("https://smn.cna.gob.mx/tools/GUI/EMAS_reporte_v2.php?id=212&formato=txt&tipo=90d", encoding = "UTF-8") %>% 
   mutate(Servicio.Meteorológico.Nacional = as.character(Servicio.Meteorológico.Nacional))
 
 # pattern <- or1(c("Altitud", "\\d+\\-\\d+\\-\\d+"))
@@ -60,7 +62,7 @@ datos3 <- as.data.frame(datos2) %>% reshape::rename(c(V1 = variables[1],
                                                       V10 = variables[10]
                                                       ))
 datos3[,1] <- strptime(datos3[,1] %>% as.character(), 
-              format = "%Y-%m-%d %H:%M")  - 21600#- (6/24) + (1&3600)
+              format = "%Y-%m-%d %H:%M")  - 21600 # Le restamos las 6 horas de diferencia con el Meridiano de Greenwich
 datos3[2:10] <- lapply(datos3[2:10], as.character)
 datos3[2:10] <- lapply(datos3[2:10], as.numeric)
 str(datos3) # Checamos estructura
@@ -75,7 +77,11 @@ precip90 <- datos3 %>%
   group_by(fecha) %>% 
   summarise(precip_por_dia = sum(na.omit(`Precipitación (mm)`))) 
 
-# Graficas 
+############
+# Graficas #
+###########
+
+# Tema propio 
 tema_juve <- theme_bw() + theme(text = element_text(family = "Asap-Bold", color = "#25636e"), 
                                 panel.grid.major = element_blank(),
                                 panel.grid.minor = element_blank(), 
@@ -88,11 +94,11 @@ tema_juve <- theme_bw() + theme(text = element_text(family = "Asap-Bold", color 
 # Grafica de radiación solar
 ggplot(data = datos3, aes(x = `AAAA/MM/DD HH:MM HORAZ`, y = `Radiación Solar (W/m²)`)) + 
   geom_line(colour = "#ff975b") + 
-  labs(title = paste0("Gráfica de radiación solar"), 
-       subtitle = paste0("Estación: Cerro Catedral (Red: SMN EMAS)"),
-       x = "Fecha (AAAA/MM/DD HH:MM)", 
+  labs(title = paste0("Gráfica de radiación solar por hora"), 
+       subtitle = paste0(datos_estacion[2], " (",datos_estacion[1], ")"),
+       x = "Fecha (MM/DD HH:MM, Año: 2019)", 
        y = "Radiación Solar", 
-       caption = "Elaboración propia con datos del SMN-Conagua. 2019.") +
+       caption = "Elaboración propia con datos del SMN-Conagua. 2019. Datos obtenidos el 25-04-2019") +
   tema_juve + 
   scale_x_datetime(labels = scales::date_format("%d/%m %H:%M"), date_breaks = "1 week") + 
   scale_y_continuous(labels = scales::dollar_format(prefix = "", suffix = " W/m\U00B2"))
@@ -101,46 +107,47 @@ ggplot(data = datos3, aes(x = `AAAA/MM/DD HH:MM HORAZ`, y = `Radiación Solar (W
 ggplot(data = datos3, aes(x = `AAAA/MM/DD HH:MM HORAZ`, y = datos3$`Humedad relativa (%)`)) + 
   geom_point(colour = "#00baaa") + 
   labs(title = paste0("Gráfica de humedad relativa"), 
-       subtitle = paste0("Estación EMAS tal"),
-       x = "Fecha (AAAA/MM/DD HH:MM)", 
+       subtitle = paste0(datos_estacion[2], " (",datos_estacion[1], ")"),
+       x = "Fecha (DD/MM HH:MM, Año: 2019)", 
        y = "Humedad Relativa (%)", 
-       caption = "Elaboración propia con datos del SMN-Conagua. 2019.") +
+       caption = "Elaboración propia con datos del SMN-Conagua. 2019. Datos obtenidos al 25-04-2019") +
   tema_juve + 
   scale_x_datetime(labels = scales::date_format("%d/%m %H:%M"), date_breaks = "1 week") + 
   scale_y_continuous(labels = scales::dollar_format(prefix = "", suffix = " %"))
 
-# # Grafica de temperatura del aire
+# Grafica de temperatura del aire
 ggplot(data = datos3, aes(x = `AAAA/MM/DD HH:MM HORAZ`, y = datos3$`Temperatura del Aire (°C)`)) +
   geom_point(colour = "#cbff5b") +
   labs(title = paste0("Gráfica de temperatura del aire"),
-       subtitle = paste0("Estación EMAS tal"),
+       subtitle = paste0(datos_estacion[2], " (",datos_estacion[1], ")"),
        x = "Fecha (AAAA/MM/DD HH:MM)",
        y = "Temperatura del aire (°C)",
-       caption = "Elaboración propia con datos del SMN-Conagua. 2019.") +
+       caption = "Elaboración propia con datos del SMN-Conagua. 2019. Datos obtenidos al 25-04-2019") +
   tema_juve +
   scale_x_datetime(labels = scales::date_format("%d/%m %H:%M"), date_breaks = "1 week") +
   scale_y_continuous(labels = scales::dollar_format(prefix = "", suffix = " °C"))
 
 # Grafica de presion atmosferica
 ggplot(data = datos3, aes(x = `AAAA/MM/DD HH:MM HORAZ`, y = datos3$`   Presión Atmosférica   `)) +
-  geom_point() + 
+  geom_point(colour = "gray") + 
   labs(title = paste0("Gráfica de presión atmosférica"), 
-       subtitle = paste0("Estación EMAS tal"),
-       x = "Fecha (AAAA/MM/DD HH:MM)", 
+       subtitle = paste0(datos_estacion[2], " (",datos_estacion[1], ")"),
+       x = "Fecha (MM/DD, Año: 2019)", 
        y = "Presión atmosférica en mmHg (milímetros de mercurio)", 
-       caption = "Elaboración propia con datos del SMN-Conagua. 2019.") +
+       caption = "Elaboración propia con datos del SMN-Conagua. 2019. Datos obtenidos al 25-04-2019") +
   tema_juve  +   
   scale_x_datetime(labels = scales::date_format("%d/%m"), date_breaks = "1 week")  + 
   scale_y_continuous(labels = scales::dollar_format(prefix = "", suffix = " mmHg"), expand = c(0,0))
 
 # Grafica de precipitacion diaria ultimos 90 dias
 ggplot(data = precip90, aes(x = fecha, y = precip_por_dia)) + 
-  geom_bar(stat = 'identity') + 
-  labs(title = paste0(""), 
-       subtitle = paste0("Estación EMAS tal"),
-       x = "Fecha (AAAA/MM/DD HH:MM)", 
+  geom_bar(stat = 'identity', fill = "#006fba") + 
+  labs(title = paste0("Gráfica de precipitación acumulada a nivel diario"), 
+       subtitle = paste0(datos_estacion[2], " (",datos_estacion[1], ")"),
+       x = "Fecha (MM/DD, Año: 2019)", 
        y = "Precipitación diaria acumulada", 
-       caption = "Elaboración propia con datos del SMN-Conagua. 2019.") +
+       caption = "Elaboración propia con datos del SMN-Conagua. 2019. Datos obtenidos al 25-04-2019") +
        tema_juve  +   
        scale_x_date(labels = scales::date_format("%d/%m"), date_breaks = "1 week")  + 
 scale_y_continuous(labels = scales::dollar_format(prefix = "", suffix = " mm"), expand = c(0,0))
+
