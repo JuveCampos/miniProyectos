fb6cc65cb6a51bb4b3101ea945d6312eed23281e
Jorge Juvenal Campos Ferreira <41249739+JuveCampos@users.noreply.github.com>
2020-09-26 11:23:04 -0500

    Modificaciones con comentarios

--- Codigo clase 6 VIS 2020/Codigo_visto_clase_6.R
+++ Codigo clase 6 VIS 2020/Codigo_visto_clase_6.R
@@ -1,22 +1,35 @@
-# Descarga datos conagua
+# Descarga datos conagua #
+# Ejemplo visto en clase
+# Jorge Juvenal Campos Ferreira 
 library(tidyverse)
 
+# La página en la cual puedes encontrar los datos es: https://smn.conagua.gob.mx/es/climatologia/informacion-climatologica/informacion-estadistica-climatologica
+# SI tienes dudas sobre como se ve una instalación para medir estos datos, puedes consultar: 
+#  https://es.wikipedia.org/wiki/Estación_meteorológica
 
-"https://smn.conagua.gob.mx/tools/RESOURCES/Diarios/9036.txt"
-"https://smn.conagua.gob.mx/tools/RESOURCES/Diarios/9068.txt"
+# Para consultar los datos de las estaciones, puedes ver estos dos ejemplos: 
+"https://smn.conagua.gob.mx/tools/RESOURCES/Diarios/9036.txt" # Playa Caleta 454, Colonia Marte, Iztacalco, Ciudad de México.
+"https://smn.conagua.gob.mx/tools/RESOURCES/Diarios/9068.txt" # Puente La llave, Del. Cuauhtemoc. CDMX
 
-estacion <- "06"
+# Usamos primero la estación de Iztacalco para el caso n = 1
+estacion <- "36"
 
+# Código generado con el asistente de importación "Import Dataset" de RStudio. 
 bd <- read_table2(paste0("https://smn.conagua.gob.mx/tools/RESOURCES/Diarios/90", estacion ,".txt"), 
                      locale = locale(encoding = "ISO-8859-1"), 
                      skip = 18)
 
+# Nota! Esto funciona si tenemos, dentro de nuestro proyecto, una carpeta llamada "01/Datos" y dentro una llamada "Datos Conagua". 
+# Si no, borra esa parte.
 write.csv(bd, paste0("01_Datos/Datos Conagua/90",estacion,".csv"))
 
-
+# ¿Qué contiene esta secuencia? 
 c(paste0("0", 1:9), 10:99)
 
+# Si no sabes para que funciona tryCatch(), revisa el video o checa la ayuda: ?tryCatch
 for(estacion in c(paste0("0", 1:9), 10:99)){
+
+  # ¿Para que sirve esta linea? ¿Qué pasa si la quito?
   print(estacion)
   tryCatch({
     
@@ -27,7 +40,7 @@ for(estacion in c(paste0("0", 1:9), 10:99)){
   write.csv(bd, paste0("01_Datos/Datos Conagua/90",estacion,".csv"))
   }, 
   error = function(e){
-    print(paste0("Algo tronó en el paso", estacion))
+    print(paste0("Algo tronó en el paso ", estacion, " T__T"))
   }
   )
 }
